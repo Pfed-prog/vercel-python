49f404f844cfec11789472388c4d72935df82103
pfed-prog <fedorovn19@gmail.com>
2022-10-03 18:02:41 +0200

    tinkering

--- index.py
+++ index.py
@@ -5,9 +5,6 @@ import requests
 from flask import Flask, Response, request, jsonify
 
 import pandas as pd
-import numpy as np
-#from statsmodels.tsa.arima.model import ARIMA
-
 
 app = Flask(__name__)
 
@@ -24,28 +21,28 @@ def home():
     if request.args.get('f'):
         forward_steps = int(request.args.get('f'))
     else:
-        forward_steps =15
+        forward_steps = 15
     if request.args.get('a'):
         address = request.args.get('a')
     else:
         address = "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984"
 
-    query = """
-        {token (id: "%s"){tokenDayData { priceUSD date } } }
-        """ % address
+    #query = """
+    #    {token (id: "%s"){tokenDayData { priceUSD date } } }
+    #   """ % address
 
-    r = requests.post(URL, json={'query': query})
-    json_data = json.loads(r.text)
-    df_data = json_data['data']['token']['tokenDayData']
-    df = pd.DataFrame(df_data)
+    #r = requests.post(URL, json={'query': query})
+    #json_data = json.loads(r.text)
+    #df_data = json_data['data']['token']['tokenDayData']
+    #df = pd.DataFrame(df_data)
 
     #df.priceUSD = df.priceUSD.replace(0, np.nan).dropna()
     #df.priceUSD = df.priceUSD.astype(float)
 
-    last = df.date.iloc[-1:].values[0]
-    timestep = df.date.iloc[-1:].values[0]-df.date.iloc[-2:-1].values[0]
+    #last = df.date.iloc[-1:].values[0]
+    #timestep = df.date.iloc[-1:].values[0]-df.date.iloc[-2:-1].values[0]
 
-    return jsonify({ 'last_date': str(last), 'timestep': str(timestep)})
+    return jsonify({ 'last_date': 123})
 @app.route('/<path:path>')
 def catch_all(path):
     """check"""
