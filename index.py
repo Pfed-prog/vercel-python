3467df43edcb3ccda20df1aa48c1ffe3fe857288
pfed-prog <fedorovn19@gmail.com>
2022-11-17 19:38:37 +0100

    retry

--- index.py
+++ index.py
@@ -6,6 +6,7 @@ import requests
 from flask import Flask, request, jsonify, send_file
 import joblib
 import pandas as pd
+import keras
 
 app = Flask(__name__)
 
@@ -82,7 +83,7 @@ def home():
     #df.iloc[:, 1:] = scaler.transform(df.iloc[:, 1:])
     #df[['priceUSD']] = y_scaler.transform(df[['priceUSD']])
 
-    model = joblib.load(join("data",  'lstm.pkl'))
+    #model = joblib.load(join("data",  'lstm.pkl'))
 
 
     #make prediction
@@ -91,18 +92,17 @@ def home():
     make_pred_data = df.iloc[-sequence_length:]
     make_pred_data = make_pred_data.values.reshape((1, sequence_length, 12))
 
-    prediction = model.predict(make_pred_data)
+    #prediction = model.predict(make_pred_data)
     last = df.date.iloc[-1:].values[0]
 
-    #return jsonify({ 'last_date': str(last)})
+    return jsonify({ 'last_date': str(last)})
 
 
-    return jsonify({ 'last_date': str(last), "prediction": str(prediction) })
+    #return jsonify({ 'last_date': str(last), "prediction": str(prediction) })
 
 @app.route('/prediction')
 def predict():
     """returns prediction"""
-    joblib.load(join("data",  'scaler.save'))
     predictions = joblib.load(join("data",  'model.pkl'))
     return jsonify({'predictions': list(predictions)})
 
